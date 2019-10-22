f9e99186122c2bab7a2e035a8fc4396896085e83
Candice Peters <candicelizabethpeters@gmail.com>
2019-10-22 21:46:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,21 @@ require_relative "../config/environment.rb"
 
 class Student
 attr_accessor :id, :name, :grade 
+
 def initialize(id=nil, name, grade)
   @id = id
   @name = name 
   @grade = grade
 end
 
+def self.create_table 
+  sql = <<-SQL 
+  CREATE TABLE students (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  grade INTEGER)
+        SQL
+        
+end
 
 end
