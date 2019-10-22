c1a077e3219a9c97f6decb7eab8b2df06f180eb9
Candice Peters <candicelizabethpeters@gmail.com>
2019-10-22 22:05:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,7 +54,7 @@ def self.new_from_db(row)
   end
   
   def self.find_by_name(name)
-    sql = "SELECT * FROM songs WHERE name = ?"
+    sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
     Song.new(result[0], result[1], result[2])
   end
