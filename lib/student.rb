d631ab7da1f67ae2577a8b039e5e81024173bbe9
Candice Peters <candicelizabethpeters@gmail.com>
2019-10-22 21:44:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,11 @@ require_relative "../config/environment.rb"
 
 class Student
 attr_accessor :id, :name, :grade 
-initialize(id=nil, name, grade)
+def initialize(id=nil, name, grade)
+  @id = id
+  @name = name 
+  @grade = grade
+end
 
 
 end
