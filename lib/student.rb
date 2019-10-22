17114a13a5b1209ce8a872e5e20b01d44a2ba8b2
Candice Peters <candicelizabethpeters@gmail.com>
2019-10-22 22:07:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,4 +58,9 @@ def self.new_from_db(row)
     result = DB[:conn].execute(sql, name)[0]
     Student.new(result[0], result[1], result[2])
   end
+  
+  def update 
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql,self.name,self.grade,self.id)
+  end
 end
