f6740c8fe5d6ed1c69d8612fb9665643c85c099f
Candice Peters <candicelizabethpeters@gmail.com>
2019-10-22 22:11:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,7 @@ def self.new_from_db(row)
     grade = row[2]
     self.new(id, name, grade)
   end 
-  end
+  
   
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
